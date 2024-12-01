04143ba58cf1746ca6f4ddfe5592c7b4623c04ef
Henri Heilala <contact@henriheilala.fi>
2024-12-01 19:43:26 +0200

    feat: solve 1b

---
+++ day1/day1b.js
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+const inputFileName = 'input.txt'
+const leftColumn = []
+const rightColumn = []
+let similarityScore = 0
+
+fs.readFileSync(inputFileName, 'utf-8').trim().split('\n')
+    .map(input => {
+        const commaSeparated = input.replaceAll('   ', ',')
+        const leftValue = parseInt(commaSeparated.split(',')[0])
+        const rightValue = parseInt(commaSeparated.split(',')[1])
+        leftColumn.push(leftValue)
+        rightColumn.push(rightValue)
+    })
+
+leftColumn.sort()
+rightColumn.sort()
+    
+leftColumn.forEach((leftValue) => {
+    const rightValueCount = rightColumn.filter(value => value == leftValue).length
+    return similarityScore += leftValue * rightValueCount
+    })
+    
+console.log(similarityScore)

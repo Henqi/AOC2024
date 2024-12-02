9e96b33e66bb6583e900d5528c287fe9b35047ad
Henri Heilala <contact@henriheilala.fi>
2024-12-03 00:21:49 +0200

    fix: remove whitespace

--- day2/day2a.js
+++ day2/day2a.js
@@ -8,13 +8,13 @@ const reports = fs.readFileSync(inputFileName, 'utf-8').trim().split('\n').map(v
 
 const isSafeAndSortedAscending = (array) => {
     return array.every((value, index) => {
-        return (index === 0 || (value > array[index-1] && thresholdMin <= Math.abs(value - array[index-1]) && Math.abs(value - array[index-1]) <= thresholdMax) )
+        return (index === 0 || (value > array[index-1] && thresholdMin <= Math.abs(value - array[index-1]) && Math.abs(value - array[index-1]) <= thresholdMax))
     })
 }
 
 const isSafeAndSortedDescending = (array) => {
     return array.every((value, index) => {
-        return (index === 0 || (value < array[index-1] && thresholdMin <= Math.abs(value - array[index-1]) && Math.abs(value - array[index-1]) <= thresholdMax) )
+        return (index === 0 || (value < array[index-1] && thresholdMin <= Math.abs(value - array[index-1]) && Math.abs(value - array[index-1]) <= thresholdMax))
     })
 }
 

5f0ef5ad7add9ae14d45f8823e45e209249a60b2
Henri Heilala <contact@henriheilala.fi>
2024-12-03 00:18:13 +0200

    feat: solve 2a

---
+++ day2/day2a.js
@@ -0,0 +1,23 @@
+import fs from 'fs'
+
+const inputFileName = 'input.txt'
+const thresholdMax = 3
+const thresholdMin = 1
+
+const reports = fs.readFileSync(inputFileName, 'utf-8').trim().split('\n').map(value => value.split(' ')).map(report => report.map(Number))
+
+const isSafeAndSortedAscending = (array) => {
+    return array.every((value, index) => {
+        return (index === 0 || (value > array[index-1] && thresholdMin <= Math.abs(value - array[index-1]) && Math.abs(value - array[index-1]) <= thresholdMax) )
+    })
+}
+
+const isSafeAndSortedDescending = (array) => {
+    return array.every((value, index) => {
+        return (index === 0 || (value < array[index-1] && thresholdMin <= Math.abs(value - array[index-1]) && Math.abs(value - array[index-1]) <= thresholdMax) )
+    })
+}
+
+const safeReports = reports.filter(report => isSafeAndSortedAscending(report) || isSafeAndSortedDescending(report)).length
+console.log(safeReports)
+

e57fad00f963b937dac7ce3a0086681a00cbaa6d
Henri Heilala <contact@henriheilala.fi>
2024-12-08 21:35:51 +0200

    chore: update eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,12 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
+import js from "@eslint/js";
 
 export default [
-    {languageOptions: {globals: globals.node}},
-    pluginJs.configs.recommended
+    js.configs.recommended,
+
+   {
+       rules: {
+           "no-unused-vars": "warn",
+           "no-undef": "warn"
+       }
+   }
 ];

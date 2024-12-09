52774387bc53bebdcab690a618806ae333dbdf3a
Henri Heilala <contact@henriheilala.fi>
2024-12-09 21:23:45 +0200

    feat: solve 5a

---
+++ day5/day5a.js
@@ -0,0 +1,34 @@
+import fs from 'fs'
+
+const inputFileName = 'input.txt'
+let correctUpdates = []
+let wrongUpdates = []
+
+const rulesAndUpdates = fs.readFileSync(inputFileName, 'utf-8').trim().split('\n')
+
+const rules = rulesAndUpdates.slice(0, [rulesAndUpdates.indexOf('')]).map(rule => rule.split('|').map(Number))
+const updates = rulesAndUpdates.slice([rulesAndUpdates.indexOf('')+1], -1).map(update => update.split(',').map(Number))
+
+const checkIfCorrectOrder = (update) => {
+  for (let i in rules) {
+    if (update.indexOf(rules[i][0]) === -1 || update.indexOf(rules[i][1]) === -1) {
+      continue 
+    }
+    if (update.indexOf(rules[i][0]) < update.indexOf(rules[i][1])) {
+    } else {
+      return false
+    }
+  }
+  return true
+}
+
+for (let i in updates) {
+  if (checkIfCorrectOrder(updates[i])) {
+    correctUpdates.push(updates[i])
+  } else {
+    wrongUpdates.push(updates[i])
+  }
+}
+
+const middlePageNumberSum = correctUpdates.map(update => update[Math.floor(update.length / 2)]).reduce((accumulator, value) => accumulator + value)
+console.log(middlePageNumberSum)
